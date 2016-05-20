661a259e91326d6407f0bef2aeed7a95eeec9180
frank <frank.wisniewski@gmail.com>
2016-05-19 23:01:50 -0700

    Added gradle instructions to generate Maven POM.

--- src/main/java/io/datanerds/avropatch/operation/Copy.java
+++ src/main/java/io/datanerds/avropatch/operation/Copy.java
@@ -1,11 +1,22 @@
 package io.datanerds.avropatch.operation;
 
-import io.datanerds.avropatch.Path;
+import com.github.fge.jackson.jsonpointer.JsonPointer;
 import org.apache.avro.reflect.AvroIgnore;
+import org.apache.avro.reflect.AvroSchema;
+import org.apache.avro.reflect.Stringable;
 
 public class Copy implements Operation {
     @AvroIgnore
     public static final String op = "copy";
-    Path path;
-    Path from;
+    @Stringable
+    @AvroSchema("\"string\"")
+    public final JsonPointer path;
+    @Stringable
+    @AvroSchema("\"string\"")
+    public final JsonPointer from;
+
+    public Copy(JsonPointer path, JsonPointer from) {
+        this.path = path;
+        this.from = from;
+    }
 }
