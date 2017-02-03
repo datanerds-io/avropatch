f3057fd86e22960dfb452634b79bf7cdf02153d9
Frank Wisniewski <frank.wisniewski@gmail.com>
2017-02-03 12:25:27 -0800

    Added some metainformation fields to Patch.

--- src/main/java/io/datanerds/avropatch/Patch.java
+++ src/main/java/io/datanerds/avropatch/Patch.java
@@ -5,39 +5,127 @@ import io.datanerds.avropatch.operation.Operation;
 import org.apache.avro.reflect.AvroSchema;
 
 import javax.annotation.Generated;
+import javax.annotation.concurrent.ThreadSafe;
 import java.util.*;
+import java.util.stream.Stream;
 
 /**
- * This class represents a JSON PATCH operation holding a sequence of operations to apply to a given object.
+ * This class represents a JSON PATCH operation holding a sequence of operations to apply to a given object. It provides
+ * shortcuts for accessing the list of operations and some additional fields for metadata information, such as the
+ * resource identifier, a timestamp and a {@link Map} holding arbitrary header information.
  *
  * @see <a href="https://tools.ietf.org/html/rfc6902">https://tools.ietf.org/html/rfc6902</a>
  */
-public class Patch {
+@ThreadSafe
+public class Patch<T> {
 
     @AvroSchema(DefaultSchema.HEADERS)
-    private final Map<String, ?> headers;
+    private final Map<String, ? super Object> headers;
+    @AvroSchema(DefaultSchema.VALUE)
+    private final T resource;
+    @AvroSchema(DefaultSchema.TIMESTAMP)
+    private final Date timestamp;
     private final List<Operation> operations;
 
     @SuppressWarnings("unused") // no-arg constructor needed by Avro
     private Patch() {
-        this(new ArrayList<>(), new HashMap<>());
+        this(null, Collections.emptyList());
     }
 
-    public Patch(List<Operation> operations) {
-        this(operations, Collections.EMPTY_MAP);
+    /**
+     * Minimalistic constructor setting the timestamp to {@code new Date()} and initializing the operation list and
+     * headers map with {@link Collections#emptyList()} / {@link Collections#emptyMap()}.
+     * @param resource resource identifier
+     */
+    public Patch(T resource) {
+        this(resource, Collections.emptyList(), Collections.emptyMap());
     }
 
-    public Patch(List<Operation> operations, Map<String, ?> headers) {
-        this.operations = new ArrayList<>(operations);
-        this.headers = new HashMap<>(headers);
+    /**
+     * This constructor sets the timestamp to {@code new Date()} and initializes the headers map with
+     * {@link Collections#emptyMap()}.
+     * @param resource resource identifier
+     * @param operations sequence of operations to apply to a object identifiable by {@code resource}
+     */
+    public Patch(T resource, List<Operation> operations) {
+        this(resource, operations, Collections.emptyMap());
     }
 
-    public List<Operation> getOperations() {
-        return Collections.unmodifiableList(operations);
+    /**
+     * This constructor sets the timestamp to {@code new Date()} and initializes the operation list with
+     * {@link Collections#emptyList()}.
+     * @param resource resource identifier
+     * @param headers arbitrary header information
+     */
+    public Patch(T resource, Map<String, ?> headers) {
+        this(resource, Collections.emptyList(), headers, new Date());
     }
 
-    public Map<String, ?> getHeaders() {
-        return Collections.unmodifiableMap(headers);
+    /**
+     * This constructor sets the timestamp to {@code new Date()}.
+     * @param resource resource identifier
+     * @param operations sequence of operations to apply to a object identifiable by {@code resource}
+     * @param headers arbitrary header information
+     */
+    public Patch(T resource, List<Operation> operations, Map<String, ?> headers) {
+        this(resource, operations, headers, new Date());
+    }
+
+    /**
+     *
+     * @param resource resource identifier
+     * @param operations sequence of operations to apply to a object identifiable by {@code resource}
+     * @param headers arbitrary header information
+     * @param timestamp timestamp
+     */
+    public Patch(T resource, List<Operation> operations, Map<String, ?> headers, Date timestamp) {
+        this.operations = Collections.unmodifiableList(new ArrayList<>(operations));
+        this.resource = resource;
+        this.timestamp = timestamp;
+        this.headers = Collections.unmodifiableMap(new HashMap<>(headers));
+    }
+
+    public Operation getOperation(int index) {
+        return operations.get(index);
+    }
+
+    public int size() {
+        return operations.size();
+    }
+
+    public Stream<Operation> stream() {
+        return operations.stream();
+    }
+
+    public boolean isEmpty() {
+        return operations.isEmpty();
+    }
+
+    public Map<String, ? super Object> getHeaders() {
+        return headers;
+    }
+
+    /**
+     * Convenient method to retrieve and cast a specific header value.
+     * @param name name of the header
+     * @param <V> expected value type for given header
+     * @return header value if exists, {@code null} otherwise
+     * @throws ClassCastException
+     */
+    public <V> V getHeader(String name) {
+        return (V) headers.get(name);
+    }
+
+    public boolean hasHeader(String name) {
+        return headers.containsKey(name);
+    }
+
+    public T getResource() {
+        return resource;
+    }
+
+    public Date getTimestamp() {
+        return timestamp;
     }
 
     @Override
@@ -49,14 +137,16 @@ public class Patch {
         if (o == null || getClass() != o.getClass()) {
             return false;
         }
-        Patch patch = (Patch) o;
+        Patch<T> patch = (Patch<T>) o;
         return Objects.equals(headers, patch.headers) &&
+                Objects.equals(resource, patch.resource) &&
+                Objects.equals(timestamp, patch.timestamp) &&
                 Objects.equals(operations, patch.operations);
     }
 
     @Override
     @Generated("IntelliJ IDEA")
     public int hashCode() {
-        return Objects.hash(headers, operations);
+        return Objects.hash(headers, resource, timestamp, operations);
     }
 }
